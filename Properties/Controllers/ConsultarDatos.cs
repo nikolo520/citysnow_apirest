575abbf5400b68032f66e71d2a411013e20bf9fd
Nicolas Murillo <uninoma_20@hotmail.com>
2021-10-05 10:47:51 -0500

    Create Controller

---
+++ Properties/Controllers/ConsultarDatos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Properties.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultarDatos : Controller
+    {
+        [HttpGet("{id}")]
+        public string Get(String city)
+        {
+            return city switch
+            {
+                
+            };
+        }
+    }
+}
