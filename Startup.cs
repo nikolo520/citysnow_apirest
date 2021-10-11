709ab11f11ede37935e7936c5382b82023b58584
Nicolas Murillo <uninoma_20@hotmail.com>
2021-10-11 15:54:45 -0500

    Agregando COnexion a la base de datos

--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apirest_webapp.Contexts;
 
 namespace apirest_webapp
 {
@@ -23,7 +26,8 @@ namespace apirest_webapp
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
+            services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
             services.AddCors();
             services.AddControllers();
         }
