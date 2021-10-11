a55936e42ec00452eff9b7e3ff1ca5b73356b9df
Nicolas Murillo <uninoma_20@hotmail.com>
2021-10-11 15:53:35 -0500

    Creando Contexto

---
+++ Contexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apirest_webapp.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Entities.Articulo> Articulo { get; set; }
+
+        public DbSet<Entities.Clima> Clima { get; set; }
+
+        public DbSet<Entities.History> History { get; set; }
+    }
+}
